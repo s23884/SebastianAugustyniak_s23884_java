ecc1d826e6fc6d8200885dd2b4dc733b2271b0ff
s23884 <74986568+s23884@users.noreply.github.com>
2022-09-04 14:26:48 +0200

    Add files via upload

---
+++ s23884-nbp/src/main/java/jazs23884nbp/s23884nbp/S23884NbpApplication.java
@@ -0,0 +1,13 @@
+package jazs23884nbp.s23884nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S23884NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S23884NbpApplication.class, args);
+	}
+
+}
